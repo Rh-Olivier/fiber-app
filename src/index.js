90017a723b2d9215d44f219292b0f458689846a4
Rasolomanana Herimanitra Olivier <rasolomananaherimanitraolivier@gmail.com>
2021-08-20 08:06:48 +0100

    ok

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from "./components/UpperCamelCase"
-import {BrowserRouter as Router} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 
 
 ReactDOM.render(
